653d4de064c2001f27d597edc6c62b96a8dc9b9b
Pedro Correa <pedro.correa@estrategia.com>
2025-02-10 10:38:50 -0300

    Refactor services.nix: remove xterm configuration and improve formatting

--- services.nix
+++ services.nix
@@ -11,11 +11,8 @@
         pipewire = {
             enable = true;
         };
-
-        # Xterm
-        xserver.desktopManager.xterm.enable = false;
     };
-
+        
     # Logitech drivers
     services.solaar.enable = true;
 }
