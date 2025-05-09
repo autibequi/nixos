cfaee2ecef1a3fc97f3a6ae44708b9c79a00fce6
Pedro Correa <pedro.correa@estrategia.com>
2025-05-09 11:06:06 -0300

    Refactor flake.lock and flake.nix: remove unused inputs and update nixpkgs source

--- flake.nix
+++ flake.nix
@@ -3,12 +3,11 @@
 
   inputs = {
     # Nix
-    nixpkgs.follows = "nixos-cosmic/nixpkgs-stable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # Others
-    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     
     # Interactive SystemD
@@ -27,7 +26,7 @@
 
   # Outputs
   # This is the default output, which is a set of attributes.
-  outputs = { self, nixpkgs, solaar, nixos-hardware, home-manager,  nixos-cosmic, chaotic, ... }@inputs: {
+  outputs = { self, nixpkgs, solaar, nixos-hardware, home-manager, chaotic, ... }@inputs: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
@@ -49,13 +48,13 @@
           home-manager.useUserPackages = true;
         }
         # Cosmic
-        nixos-cosmic.nixosModules.default
-        {
-          nix.settings = {
-            substituters = [ "https://cosmic.cachix.org/" ];
-            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-          };
-        }
+        # nixos-cosmic.nixosModules.default
+        # {
+        #   nix.settings = {
+        #     substituters = [ "https://cosmic.cachix.org/" ];
+        #     trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+        #   };
+        # }
         # Interactive SystemD
         {
           environment.systemPackages = [ inputs.isd.packages.x86_64-linux.default ];
