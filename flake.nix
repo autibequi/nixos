099e728972de16860cb4dd8e1f8a97a078517cbb
Pedro Correa <pedro.correa@estrategia.com>
2025-05-12 14:09:13 -0300

    Refactor flake.nix: reorganize input sources and update nixpkgs reference; add cosmic module support Refactor configuration.nix: enable cosmic desktop environment and disable gnome Update launch.json: add Nix Flake Update command for easier flake management Update flake.lock: refresh dependencies and update hashes for improved stability Update nix.nix: modify substituters and trusted public keys for enhanced caching

--- flake.nix
+++ flake.nix
@@ -1,42 +1,25 @@
 {
   description = "A very basic flake";
-
   inputs = {
     # Nix Channels
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     # nixpkgs.url = "github:NixOS/nixpkgs/staging";
     nixpkgs.url = "github:NixOS/nixpkgs/staging-next";
-
-    # Hardware URL
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  };
-
-  # Non Nixos Sources
-  inputs = {
-    # Others
-    # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    nixpkgs.follows = "nixos-cosmic/nixpkgs";
     
-    # Interactive SystemD
-    isd.url = "github:isd-project/isd";
-
-    # Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11"; 
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Logitech Solaar
-    solaar = {
-      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Other Inputs
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # Hardware
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    isd.url = "github:isd-project/isd"; # Interactive SystemD
+    home-manager.url = "github:nix-community/home-manager/release-24.11";  # Home Manager
+    solaar.url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # Logitech Solaar
   };
 
   # Outputs
   # This is the default output, which is a set of attributes.
-  outputs = { self, nixpkgs, solaar, nixos-hardware, home-manager, chaotic, ... }@inputs: {
+  outputs = { self, nixpkgs, solaar, nixos-hardware, home-manager, chaotic, nixos-cosmic, ... }@inputs: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
@@ -54,21 +37,16 @@
         chaotic.nixosModules.nyx-overlay
         chaotic.nixosModules.nyx-registry
         
+        # Cosmic
+        nixos-cosmic.nixosModules.default
+
         # home-manager
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
         }
-        
-        # Cosmic
-        # nixos-cosmic.nixosModules.default
-        # {
-        #   nix.settings = {
-        #     substituters = [ "https://cosmic.cachix.org/" ];
-        #     trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-        #   };
-        # }
+
         
         # Interactive SystemD
         {
--- nix.nix
+++ nix.nix
@@ -9,17 +9,21 @@
   # e reduzir o uso de largura de banda
   nix.settings = {
     substituters = [
-      "https://aseipp-nix-cache.global.ssl.fastly.net"
+      "https://cosmic.cachix.org/" 
       "https://chaotic-nyx.cachix.org/"
-      "https://nix-community.cachix.org"
+      "https://aseipp-nix-cache.global.ssl.fastly.net"
       "https://cache.nixos.org/"
-      
+      "https://nix-community.cachix.org"
     ];
-    
+
     trusted-public-keys = [
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" 
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
+      "aseipp-nix-cache.global.ssl.fastly.net-1:0x2a3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n="
+      "chaotic-nyx.cachix.org-1:0x2a3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n="
+      "nixos-unstable.cachix.org-1:0x2a3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n="
     ];
   };
 
