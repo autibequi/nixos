055006b250e4337c610f66f354b767e06f052da1
Pedro Correa <pedro.correa@estrategia.com>
2025-05-12 10:52:50 -0300

    Refactor Plymouth configuration: update theme to "dragon" and include additional selected themes

--- plymouth.nix
+++ plymouth.nix
@@ -3,11 +3,12 @@
 
     plymouth = {
       enable = true;
-      theme = "abstract_ring_alt";
+      theme = "dragon";
       themePackages = with pkgs; [
         # By default we would install all themes
+        # https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/by-name/ad/adi1090x-plymouth-themes/shas.nix
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "abstract_ring_alt" ];
+          selected_themes = [ "abstract_ring_alt" "dragon" "connect" ];
         })
       ];
     };
