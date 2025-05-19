9c32fa0c151346630e42befa81de202d0ecea134
Pedro Correa <pedro.correa@estrategia.com>
2025-05-19 16:06:58 -0300

    Refactor kernel parameters and enhance battery power management settings

--- modules/battery.nix
+++ modules/battery.nix
@@ -2,12 +2,42 @@
 
 {
   # NixOs stock power management
-  powerManagement = {
-    # Habilitar powertop para análise e otimização de energia
-    powertop.enable = true;
+  powerManagement.powertop.enable = true;
 
-    # Devido ao uso de SSDs NVMe, o modo de energia "med_power_with_dipm" é mais adequado
-    # poupando o disco para maior desempenho a longo prazo devido a heating e permitindo sleep.
-    scsiLinkPolicy = "med_power_with_dipm"; 
+  # Desabilitar power-profiles-daemon para evitar conflitos com o TLP
+  services.power-profiles-daemon.enable = false;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Throttle CPU on battery
+      CPU_MAX_PERF_ON_BAT = 10;
+
+      # Boost CPU on AC
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      # Use power policy on battery
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      # Throttle GPU on battery
+      RADEON_POWER_PROFILE_ON_AC = "high";
+      RADEON_POWER_PROFILE_ON_BAT = "low";
+
+      # Keep WiFi on battery
+      WIFI_PWR_ON_BAT = "on";
+    };
   };
+
+
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=yes
+    AllowHibernation=no
+  '';
+
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
 }
