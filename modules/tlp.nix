9e97ca4cf6f4e97c3e6dc060dcc44703f37f9d71
Pedro Correa <pedro.correa@estrategia.com>
2025-05-20 10:05:57 -0300

    Update TLP configuration with CPU energy performance policies for improved power management

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -39,6 +39,10 @@
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
+      # CPU Energy Policy
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
       #ASPM
       PCIE_ASPM_ON_AC = "performance";
       PCIE_ASPM_ON_BAT = "powersupersave";
