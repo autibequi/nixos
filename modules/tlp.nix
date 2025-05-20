0d4ec650c596533e26998a4fa730f3bf796a5d3b
Pedro Correa <pedro.correa@estrategia.com>
2025-05-20 14:23:36 -0300

    Update TLP configuration for CPU scaling governor and driver operation modes

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -13,18 +13,22 @@
       # AC (conectado à energia)
       # cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
       ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_BOOST_ON_AC = 1; 
+      CPU_MIN_PERF_ON_AC=0;
+      CPU_MAX_PERF_ON_AC=100;
 
       # Battery (na bateria)
       ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_BOOST_ON_BAT = 0; # pode ser util devido a scx_lavd que agrupa as threads em poucos cores??
+      CPU_MIN_PERF_ON_BAT=0;
+      CPU_MAX_PERF_ON_BAT=30;
 
       # CPU Driver
       # must be passive/guided for userland schedulers
       # but active is better for battery life
-      CPU_DRIVER_OPMODE_ON_AC = "guided";
+      CPU_DRIVER_OPMODE_ON_AC = "passive";
       CPU_DRIVER_OPMODE_ON_BAT = "active"; 
 
       # Desabilitar o watchdog do kernel
