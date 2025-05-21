356faf1969dc3d42fcbbda3b7c24f1be621191e1
Pedro Correa <pedro.correa@estrategia.com>
2025-05-20 22:03:06 -0300

    Atualiza o parâmetro "amd_pstate" no arquivo kernel.nix de "active" para "guided", ajustando a configuração do gerenciamento de energia para processadores AMD.

--- core/kernel.nix
+++ core/kernel.nix
@@ -16,7 +16,7 @@
     "usb-storage.quirks=0x152d:0x0583:i"
 
     # Força o uso do p-state ativo para o processador AMD
-    "amd_pstate=active" 
+    "amd_pstate=guided" 
   ];
 
   # Configurar compressão.
