17ae3fe5f54e02add21106e4f93602382017f6b1
Pedro Correa <pedro.correa@estrategia.com>
2025-05-29 14:28:26 -0300

    Remove fastboot from kernel parameters

--- core/kernel.nix
+++ core/kernel.nix
@@ -15,8 +15,6 @@
   security.protectKernelImage = false;
 
   boot.kernelParams = [
-    # those actually do something
-    "fastboot" # faster boot
     "amdgpu.dcdebugmask=0x10" # refresh issues https://gitlab.gnome.org/GNOME/mutter/-/issues/3299\
 
     # Force UAS for external NVME USB-C case; this garantees high speed mode | lsusb -t:
