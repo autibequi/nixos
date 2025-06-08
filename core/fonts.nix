0707b2eec87847598a235849b6fc30d0e5c2d8dd
Pedro Correa <pedro.correa@estrategia.com>
2025-06-08 09:37:14 -0300

    Add Noto Color Emoji font and update fontconfig settings

--- core/fonts.nix
+++ core/fonts.nix
@@ -5,20 +5,28 @@
     fontconfig.enable = true;
     enableDefaultPackages = false;
     packages = with pkgs; [
-      # nerd-fonts
-      nerd-fonts.fira-code
       nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      noto-fonts-emoji
     ];
   };
 
+  fonts.fontconfig.useEmbeddedBitmaps = true;
+
   fonts.fontconfig.defaultFonts = {
     monospace = [
+      "JetBrainsMono Nerd Font"
+      "Noto Color Emoji"
       "FiraCode Nerd Font"
     ];
     sansSerif = [
       "JetBrainsMono Nerd Font"
+      "Noto Color Emoji"
+      "FiraCode Nerd Font"
     ];
     serif = [
+      "JetBrainsMono Nerd Font"
+      "Noto Color Emoji"
       "FiraCode Nerd Font"
     ];
   };
