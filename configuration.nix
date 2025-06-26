417ed98158d21e0dc54ba9aec6c864b326b21029
Pedro Correa <pedro.correa@estrategia.com>
2025-06-26 15:45:05 -0300

    update and split configuration and setup

--- configuration.nix
+++ configuration.nix
@@ -5,43 +5,18 @@
 # Instalation
 # Just setup the root, boot and swap partitions
 # If you dont want swap or hibernation, just comment out the swapDevices and boot.resumeDevice
+#
+# This file is locked by .gitinore wont be commitable to avoid conflits.
+# Use setup.nix to enable ore disable packages
 let
   swapDiskUUID = "/dev/disk/by-uuid/c824afe8-bf19-4f7f-9876-5fcff8c93593";
   bootDiskUUID = "/dev/disk/by-uuid/6B74-DC9D";
   rootDiskUUID = "/dev/disk/by-uuid/4265d4f9-7f7b-4ebf-a3b4-a3406c3c0955";
 in
 {
-  # System State Version
-  system.stateVersion = "25.05";
-
+  # Importa Setup do Usuario
   imports = [
-    # Core
-    ./core/nix.nix
-    ./core/core.nix
-    ./core/kernel.nix
-    ./core/home.nix
-    ./core/services.nix
-    ./core/programs.nix
-    ./core/packages.nix
-    ./core/shell.nix
-    ./core/fonts.nix
-
-    # Extra
-    ./modules/ai.nix
-    ./modules/asus.nix
-    ./modules/battery.nix
-    ./modules/bluetooth.nix
-    # ./modules/podman.nix
-    ./modules/nvidia.nix
-    ./modules/plymouth.nix
-    # ./modules/howdy.nix
-    # ./modules/flatpak.nix
-    ./modules/work.nix
-
-    # Desktop Environments
-    ./modules/gnome/core.nix
-    ./modules/cosmic.nix
-    # ./modules/kde.nix
+    ./setup.nix
   ];
 
   # Hibenration and swap are optional and can be commented out
