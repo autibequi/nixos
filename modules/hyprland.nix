93c935dea2819ed2b98f365619559ce0934de8a4
Pedro Correa <pedro@autibequi.com>
2025-07-05 22:44:02 -0300

    feat: Add hyprlock configuration and bindings
    This commit adds configuration for hyprlock, which includes
    customizations for the lock screen appearance and behavior. It also adds
    a keybinding to launch hyprlock.
    
    Additionally, this commit adds a configuration file for hyprshade.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -119,8 +119,8 @@
     };
 
     home.file = {
-      # ".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
-      # ".config/hypr/hyprshade.toml".source = ./hypr/hyprshade.toml;
+      ".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
+      ".config/hypr/hyprshade.toml".source = ./hypr/hyprshade.toml;
       ".config/waybar/config.jsonc".source = ../dotfiles/hyprland/waybar.json;
       ".config/waybar/style.css".source = ../dotfiles/hyprland/waybar.css;
     };
