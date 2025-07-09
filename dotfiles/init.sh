f7d3d3d1654b299d24735c253c8d65c8e5694864
Pedro Correa <pedro@autibequi.com>
2025-07-08 21:53:55 -0300

    Add new configurations for Hyprland and Waybar
    - Introduced a script to toggle GTK dark mode in init.sh.
    - Added new configuration files for Dunst, Hypridle, Hyprlock, and Hyprshade.
    - Created Waybar configuration and CSS for enhanced visual styling.
    - Removed obsolete toggle-theme.sh and Wofi styles.
    - Cleaned up tasks.json by removing an empty file.

--- dotfiles/init.sh
+++ dotfiles/init.sh
@@ -74,3 +74,21 @@ command_not_found_handler() {
 }
 
 alias secret=vi ~/secrets.sh
+
+toggle-gtk-darkmode {
+  # Get current theme
+  current_theme=$(gsettings get org.gnome.desktop.interface color-scheme)
+
+  # Toggle between dark and light themes
+  if [[ "$current_theme" == "'prefer-dark'" ]]; then
+      # Switch to light theme
+      gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+      gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
+      echo "Switched to light theme"
+  else
+      # Switch to dark theme
+      gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+      gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
+      echo "Switched to dark theme"
+  fi
+}
