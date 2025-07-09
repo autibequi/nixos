ab68f897b0ab27c42f71c683aaef5834a927e245
Pedro Correa <pedro@autibequi.com>
2025-07-08 23:46:31 -0300

    update base configuration

--- configuration.nix
+++ configuration.nix
@@ -13,12 +13,15 @@
     ./core/services.nix
     ./core/programs.nix
     ./core/packages.nix
+    ./core/fonts.nix
     ./core/shell.nix
+    ./core/kernel.nix
     ./modules/battery.nix
     ./modules/bluetooth.nix
     ./modules/plymouth.nix
-    ./core/fonts.nix
-    ./core/kernel.nix
+    ./modules/flatpak.nix
+    ./modules/ai.nix
+    # ./modules/podman.nix
 
     # Hardware specific
     ./modules/asus.nix
@@ -31,12 +34,6 @@
     # ./modules/kde.nixs
 
     # Extra
-    ./modules/ai.nix
-    ./modules/asus.nix
-    ./modules/bluetooth.nix
-    # ./modules/podman.nix
-    # ./modules/howdy.nix
-    ./modules/flatpak.nix
     # ./modules/work.nix
   ];
 
@@ -44,15 +41,15 @@
   options.diskUUIDs = {
     boot = lib.mkOption {
       description = "Boot partition";
-      default = "/dev/disk/by-uuid/6B74-DC9D";
+      default = "/dev/disk/by-uuid/";
     };
     root = lib.mkOption {
-      description = "Toot partition";
-      default = "/dev/disk/by-uuid/4265d4f9-7f7b-4ebf-a3b4-a3406c3c0955";
+      description = "Root partition";
+      default = "/dev/disk/by-uuid/";
     };
     swap = lib.mkOption {
       description = "Swap partition";
-      default = "/dev/disk/by-uuid/c824afe8-bf19-4f7f-9876-5fcff8c93593";
+      default = "/dev/disk/by-uuid/";
     };
   };
 }
