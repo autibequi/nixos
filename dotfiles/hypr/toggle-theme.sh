1c4a2805e72dc525aa9026047e4ec3f0e38d5535
Pedro Correa <pedro@autibequi.com>
2025-07-09 10:30:47 -0300

    Refactor theme toggle script to use gsettings directly and update portal dependencies
    - Simplified toggle-theme.sh to directly retrieve the current theme state using gsettings, removing the need for temporary state management.
    - Adjusted theme switching logic to handle the new gsettings output.
    - Updated hyprland.nix to include xdg-desktop-portal-hyprland and xdg-desktop-portal-wlr for enhanced functionality.

--- dotfiles/hypr/toggle-theme.sh
+++ dotfiles/hypr/toggle-theme.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
 
-# Garante acesso ao DBus da sessão para o gsettings funcionar via Hyprland
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
-export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+current_state=$(gsettings get org.gnome.desktop.interface color-scheme)
 
-# Hyperland cant read directly from gsettings for whatever reason
-state_file="/tmp/theme"
-
-if [ -f "$state_file" ]; then
-    current_state=$(cat "$state_file")
-else
-    current_state="dark"
-fi
-
-# Alterna usando o estado salvo
-if [ "$current_state" = "dark" ]; then
-    # Muda para light
+if [ "$current_state" = "'prefer-dark'" ]; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
-    echo "Switched to light theme"
     swww img ~/.wallpapers/light.jpg
-    echo "light" > "$state_file"
 else
-    # Muda para dark
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
-    echo "Switched to dark theme"
     swww img ~/.wallpapers/dark.jpg
-    echo "dark" > "$state_file"
 fi
--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -51,11 +51,11 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
+      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-wlr
     ];
-    config.common.default = "*";
   };
 
   # Hyprland config
