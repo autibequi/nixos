1d12de08ea0f9fff7e75f57f5cbee7f4200ae564
Pedro Correa <pedro.correa@estrategia.com>
2025-07-10 08:06:19 -0300

    Update battery.nix

--- modules/battery.nix
+++ modules/battery.nix
@@ -5,11 +5,6 @@
     powertop # power management
   ];
 
-  imports = [
-    # Too much trouble, pstate does the same with better perf
-    ./tlp.nix
-  ];
-
   # Tweak CFS latency parameters when going on/off battery
   services.system76-scheduler.settings.cfsProfiles.enable = true;
 
@@ -18,20 +13,4 @@
 
   # AMD EPP to change the power profile so pstate can change
   services.auto-epp.enable = true;
-
-  services.logind = {
-    lidSwitch = "suspend-then-hibernate";
-    powerKey = "hibernate";
-    powerKeyLongPress = "poweroff";
-  };
-
-  systemd.sleep.extraConfig = ''
-    HibernateOnACPower=true
-    HibernateDelaySec=30m
-    SuspendState=mem
-  '';
-
-  boot.kernelParams = [
-    "mem_sleep_default=deep"
-  ];
 }
