67fd2d80843bbcdd8cf39a2d223f7f203c1dcc24
Pedro Correa <pedro@autibequi.com>
2025-07-19 19:52:16 -0300

    Enhance Podman configuration by adding additional container registries and extra packages for improved functionality, while removing outdated policy settings.

--- modules/podman.nix
+++ modules/podman.nix
@@ -12,20 +12,20 @@
         "docker.io"
         "quay.io"
         "registry.fedoraproject.org"
+        "ghcr.io"
+        "registry.gitlab.com"
       ];
-      policy = {
-        default = [
-          {
-            type = "insecureAcceptAnything";
-          }
-        ];
-      };
     };
 
     podman = {
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
+      extraPackages = with pkgs; [
+        podman-compose
+        dive
+        podman-tui
+      ];
     };
   };
 
@@ -34,24 +34,6 @@
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
     podman-compose # start group of containers for dev
+    docker-compose # fallback docker compose
   ];
-
-  # TODO: Manually add docker.io to the registries.conf
-  # the setup in `virtualisation.containers.registries.search` does not work for some reasong
-  home-manager.users.pedrinho = {
-    home.file.".config/containers/registries.conf".text = ''
-      [registries.search]
-      registries = ['docker.io']
-    '';
-
-    home.file.".config/containers/policy.json".text = ''
-      {
-        "default": [
-          {
-            "type": "insecureAcceptAnything"
-          }
-        ]
-      }
-    '';
-  };
 }
