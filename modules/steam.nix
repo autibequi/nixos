b67fbef5ad1f3c5276dcdc9c966a0e0958415b51
Pedro Correa <pedro@autibequi.com>
2025-07-19 19:51:16 -0300

    Refactor Steam configuration to enable Gamescope support and Xbox controller compatibility, enhancing gaming experience.

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,8 +1,26 @@
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+  # Wiki: 
+  # https://nixos.wiki/wiki/Steam
+
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
   };
-  programs.steam.gamescopeSession.enable = true;
+
+  # Enable Xbox controller support
+  # hardware.xone.enable = true; 
+
+  # Enable AppImage support
+  # programs.appimage.enable = true;
+  # programs.appimage.binfmt = true;
 }
