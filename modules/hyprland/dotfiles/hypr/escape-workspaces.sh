7c788a2b226b88efd722f2d418d555d54f94052a
Pedro Correa <pedro@autibequi.com>
2025-08-01 01:33:19 -0300

    Update dunst.conf, escape-workspaces.sh, land.conf, and waybar.css for enhanced aesthetics and functionality
    - Modified dunst.conf for improved visual styling and adjusted notification dimensions.
    - Updated escape-workspaces.sh to streamline workspace switching and added workspace number handling.
    - Enhanced land.conf with refined border sizes and improved keybindings for workspace management.
    - Updated waybar.css to introduce CRT effects for workspace buttons and special workspace highlighting.

--- modules/hyprland/dotfiles/hypr/escape-workspaces.sh
+++ modules/hyprland/dotfiles/hypr/escape-workspaces.sh
@@ -4,11 +4,18 @@ ACTIVE_SPECIAL_WORKSPACE=$(hyprctl monitors -j | jq -r '.[] | .specialWorkspace.
 echo "ACTIVE_SPECIAL_WORKSPACE: $ACTIVE_SPECIAL_WORKSPACE"
 
 if [[ "$ACTIVE_SPECIAL_WORKSPACE" =~ ^special:.*$ ]]; then
-    # good old hack is simple and works
-    hyprctl dispatch togglespecialworkspace "bye!"
-    hyprctl dispatch togglespecialworkspace "bye!"
+    # remove special:
+    clean_name=$(echo "$ACTIVE_SPECIAL_WORKSPACE" | sed 's/^special://')
+    hyprctl dispatch togglespecialworkspace "$clean_name"
 
 else
     # Envia um evento de tecla ESC para o bus do Hyprland (simula pressionar ESC)
     echo "No special workspace found"
 fi
+
+workspace_number="${1}"
+
+echo "workspace_number: $workspace_number"
+
+# move to workspace from argument
+hyprctl dispatch workspace "$workspace_number"
