040c40b32af0e165c229b4f90c5906d2ceef331a
Pedro Correa <pedro.correa@estrategia.com>
2025-08-03 09:53:37 -0300

    Remove obsolete configuration files and add new monitor utility script
    - Deleted outdated configuration files for Atuin, Fastfetch, and Ghostty.
    - Introduced a new script `hyperutils.sh` for managing workspace transitions in Hyprland.
    - Updated `hyprland.conf` to include a fallback monitor configuration and commented out the previous monitor source.
    - Added a new template for monitor configurations while removing the old monitor configuration file.

---
+++ stow/hyprland/.config/hypr/hyperutils.sh
@@ -0,0 +1,19 @@
+escape_workspace() {
+    # Função para sair de um workspace especial e ir para o workspace desejado
+    local workspace_number="$1"
+    local active_special_workspace
+    active_special_workspace=$(hyprctl monitors -j | jq -r '.[] | .specialWorkspace.name')
+    echo "ACTIVE_SPECIAL_WORKSPACE: $active_special_workspace"
+    echo "workspace_number: $workspace_number"
+
+    if [[ "$active_special_workspace" =~ ^special:.*$ ]]; then
+        # remove o prefixo special:
+        local clean_name
+        clean_name=$(echo "$active_special_workspace" | sed 's/^special://')
+        echo "clean_name: $clean_name"
+        hyprctl dispatch togglespecialworkspace "$clean_name"
+    fi
+
+    # move para o workspace passado como argumento
+    hyprctl dispatch workspace "$workspace_number"
+}
