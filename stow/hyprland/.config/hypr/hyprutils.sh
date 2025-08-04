fa61719677a2e2649ad01cee71485c8d219602c2
Pedro Correa <pedro.correa@estrategia.com>
2025-08-04 08:54:27 -0300

    Update hyprutils.sh

--- stow/hyprland/.config/hypr/hyprutils.sh
+++ stow/hyprland/.config/hypr/hyprutils.sh
@@ -3,20 +3,13 @@
 workspace_switch() {
     # Função para sair de um workspace especial e ir para o workspace desejado
     local workspace_number="$1"
-    local active_special_workspace
-    active_special_workspace=$(hyprctl monitors -j | jq -r '.[] | .specialWorkspace.name')
-    echo "ACTIVE_SPECIAL_WORKSPACE: $active_special_workspace"
-    echo "workspace_number: $workspace_number"
-
 
     if [[ "$workspace_number" =~ ^special:.*$ ]]; then
         # Função para alternar para um workspace especial e registrar o último workspace especial acessado
         withoutSpecialWorkspace=$(echo "$workspace_number" | sed 's/special://')
-        notify-send "Switching to workspace $workspace_number"
         echo "$withoutSpecialWorkspace" > /tmp/last_special_workspace
         hyprctl dispatch togglespecialworkspace "$withoutSpecialWorkspace"
     else
-        notify-send "Workspace Switch" "Switching to workspace $workspace_number"
         # move para o workspace passado como argumento
         hyprctl dispatch workspace "$workspace_number"
     fi
