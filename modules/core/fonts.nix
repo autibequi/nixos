7c6d1c23d82287df612d5bee75370bbe3e75e967
Pedro Correa <pedro.correa@estrategia.com>
2025-11-08 22:53:47 -0300

    Update core font configuration to enable default packages and include additional font options for improved typography and editor appearance.

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -3,32 +3,57 @@
   # Fonts
   fonts = {
     fontconfig.enable = true;
-    enableDefaultPackages = false;
+    enableDefaultPackages = true;
     packages = with pkgs; [
+      # General Fonts
+      corefonts
+
+      # Monospace fonts
       nerd-fonts.jetbrains-mono
       nerd-fonts.fira-code
+      fira-code
+      jetbrains-mono
+      meslo-lgs-nf # Font for p10k theme
+
+      # Noto fonts
       noto-fonts
-      noto-fonts-cjk-compact
       noto-fonts-emoji
+
+      # CJK Fonts
+      # source-han-serif
+      # source-han-sans
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      wqy_zenhei
+      wqy_microhei
     ];
   };
 
   fonts.fontconfig.useEmbeddedBitmaps = true;
 
+  fonts.fontconfig.subpixel.rgba = "rgb";
+
   fonts.fontconfig.defaultFonts = {
     monospace = [
+      "MesloLGS NF"
       "JetBrainsMono Nerd Font"
       "FiraCode Nerd Font"
     ];
     sansSerif = [
       "Noto Sans"
-      "Noto Sans CJK SC"
+      "Source Han Sans SC"
+      "Source Han Sans TC"
+      "Source Han Sans JP"
       "DejaVu Sans"
+      "Noto Sans CJK SC"
     ];
     serif = [
       "Noto Serif"
-      "Noto Serif CJK SC"
+      "Source Han Serif SC"
+      "Source Han Serif TC"
+      "Source Han Serif JP"
       "DejaVu Serif"
+      "Noto Serif CJK SC"
     ];
   };
 }
