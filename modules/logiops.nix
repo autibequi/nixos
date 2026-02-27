2dfeada7c76f2df79310b2aa41984dde31989b83
Pedro Correa <pedro.correa@estrategia.com>
2026-02-27 12:59:01 -0300

    Refactor logiops.nix to use user-specific config path for logid service and add logid.cfg for MX Master 3 configuration.

--- modules/logiops.nix
+++ modules/logiops.nix
@@ -1,22 +1,23 @@
 {
   pkgs,
+  config,
   ...
 }:
+let
+  user = "pedrinho";
+  configPath = "${config.users.users.${user}.home}/.config/logiops/logid.cfg";
+in
 {
-  environment.systemPackages = with pkgs; [
-    logiops
-  ];
-
-  environment.etc."logid.cfg".source = ./logid.cfg;
+  environment.systemPackages = with pkgs; [ logiops ];
 
   systemd.services.logiops = {
-    description = "Unofficial userspace driver for HID++ Logitech devices (MX Master 3)";
+    description = "Unofficial userspace driver for HID++ Logitech devices";
     wantedBy = [ "multi-user.target" ];
     after = [ "multi-user.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${pkgs.logiops}/bin/logid -c /etc/logid.cfg";
       Restart = "on-failure";
+      ExecStart = "${pkgs.logiops}/bin/logid -c ${configPath}";
     };
   };
 }
