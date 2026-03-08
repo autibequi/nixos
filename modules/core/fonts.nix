e93cda4a0f7606822c81343c4927b25989650bac
Pedro Correa <pedro.correa@estrategia.com>
2026-03-08 15:08:36 -0300

    Update fonts.nix

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -19,13 +19,8 @@
       noto-fonts
       noto-fonts-color-emoji
 
-      # CJK Fonts
-      # source-han-serif
-      # source-han-sans
+      # CJK (mínimo para fallback de caracteres)
       noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      wqy_zenhei
-      wqy_microhei
     ];
   };
 
@@ -41,19 +36,12 @@
     ];
     sansSerif = [
       "Noto Sans"
-      "Source Han Sans SC"
-      "Source Han Sans TC"
-      "Source Han Sans JP"
       "DejaVu Sans"
       "Noto Sans CJK SC"
     ];
     serif = [
       "Noto Serif"
-      "Source Han Serif SC"
-      "Source Han Serif TC"
-      "Source Han Serif JP"
       "DejaVu Serif"
-      "Noto Serif CJK SC"
     ];
   };
 }
