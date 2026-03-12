36abdcc5a8ff9394b124d4fed3067cf1eb10f734
Pedro Correa <pedro.correa@estrategia.com>
2026-03-12 16:28:35 -0300

    Update podman.nix

--- modules/podman.nix
+++ modules/podman.nix
@@ -8,6 +8,8 @@
     defaultNetwork.settings.dns_enabled = true;
   };
 
+  environment.sessionVariables.DOCKER_HOST = "unix:///run/podman/podman.sock";
+
   environment.systemPackages = with pkgs; [
     docker-compose
     lazydocker
